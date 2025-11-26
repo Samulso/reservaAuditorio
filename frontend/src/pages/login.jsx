b83fe38fe6ee77024ddc5e57d239f474e18407e5
Samulso <sohfsamuelhenrique@gmail.com>
2025-11-26 17:37:10 -0300

    ...

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import "../styles/login.css";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -14,28 +14,37 @@ function Login() {
     return (
         <div className="login-container">
             <img src="../src/assets/img/senac-logo.jpg" alt="" />
-            <h2>Acesso ao Ambiente Virtual</h2>
+            <div className="title-linha">
+                <h2>Acesso ao Ambiente Virtual</h2>
+                <div className="linha"></div>
+            </div>
 
-            <div className="linha"></div>
 
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                <div className="form">
+                    <div className="inputs-label">
+                        <div>
+                            <label htmlFor="email">Email institucional:</label>
+                            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        </div>
+                    </div>
+                    <div className="inputs-label">
+                        <div>
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-type="password"
-id="password"
-value={password}
-onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
+
+
+
+                <div className="register-field">
+                    <h3>Não possui uma conta? </h3> <a className="sublinhado"href="/register">Registre-se</a>
                 </div>
-                <button type="submit">Login</button>
+
+                <button className="button-1" type="submit">Login</button>
             </form>
-        </div>
+        </div>  
     );
 }
 
